9acbf103b123670822d73b5539abad3f205ebc26
jovi-bih <bihjovi22@gmail.com>
2024-03-16 16:08:43 -0400

    modified main

--- terraform_files/main.tf
+++ terraform_files/main.tf
@@ -66,66 +66,71 @@ resource "aws_instance" "my_ec2_instance1" {
   }
 
   tags = {
-    Name = "MASTER-SERVER"
+    Name = "JENKINS-SERVER"
   }
 
   user_data = <<-EOF
     #!/bin/bash
     # wait for 1min before EC2 initialization
-    sleep 60
+    #sleep 60
+   #Install Git 
+    sudo yum install git -y
+    # Install Maven
     sudo wget https://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O /etc/yum.repos.d/epel-apache-maven.repo
     sudo sed -i s/\$releasever/6/g /etc/yum.repos.d/epel-apache-maven.repo
     sudo yum install -y apache-maven
     sudo yum install java-1.8.0-devel -y
-  EOF
+  
 
   # STEP3: USING REMOTE-EXEC PROVISIONER TO INSTALL TOOLS
-  provisioner "remote-exec" {
+  #provisioner "remote-exec" {
     # ESTABLISHING SSH CONNECTION WITH EC2
-    connection {
-      type        = "ssh"
-      private_key = file("./joykey.pem") # replace with your key-name 
-      user        = "ec2-user"
-      host        = self.public_ip
-    }
+    #connection {
+      #type        = "ssh"
+      #private_key = file("./joykey.pem") # replace with your key-name 
+      #user        = "ec2-user"
+      #host        = self.public_ip
+    #}
 
-    inline = [        
+    #inline = [
       # wait for 200sec before EC2 initialization
-      "sleep 200",
-      # Install Git 
-      "sudo yum install git -y",
-      
+      #"sleep 200",
+      #Install Git 
+      #"sudo yum install git -y",
+      #"chmod 400 joykey.pem",
+
       # Install Jenkins 
       # REF: https://www.jenkins.io/doc/tutorials/tutorial-for-installing-jenkins-on-AWS/
-      "sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo",
-      "sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key",
-      "sudo yum install java-17-amazon-corretto -y",
-      "sudo yum install jenkins -y",
-      "sudo systemctl enable jenkins",
-      "sudo systemctl start jenkins",
+      sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
+      sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
+      sudo yum install java-17-amazon-corretto -y
+      sudo yum install jenkins -y
+      sudo systemctl enable jenkins
+      sudo systemctl start jenkins
+
 
       # Install Docker
       # REF: https://docs.aws.amazon.com/serverless-application-model/latest/developerguide/install-docker.html
-      "sudo yum update -y",
-      "sudo yum install docker -y",
-      "sudo systemctl start docker",
-      "sudo systemctl enable docker",
-      "sudo usermod -aG docker jenkins",
+      sudo yum update -y
+      sudo yum install docker -y
+      sudo systemctl start docker
+      sudo systemctl enable docker
+      sudo usermod -aG docker jenkins
       # To avoid below permission error
       # Got permission denied while trying to connect to the Docker daemon socket at unix:///var/run/docker.sock
-      "sudo chmod 666 /var/run/docker.sock",
+      sudo chmod 666 /var/run/docker.sock
 
       # Install Trivy
       # REF: https://aquasecurity.github.io/trivy/v0.18.3/installation/
-      "sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm",
+      sudo rpm -ivh https://github.com/aquasecurity/trivy/releases/download/v0.18.3/trivy_0.18.3_Linux-64bit.rpm
 
       # Install Ansible
-      "sudo yum update -y",
-      "sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y",
-      "sudo yum install git python python-devel python-pip openssl ansible -y",
-    ]
+      sudo yum update -y
+      sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm -y
+      sudo yum install git python python-devel python-pip openssl ansible -y
+    EOF
+    #]
   }
-}
 
 # STEP3: OUTPUT PUBLIC IP OF EC2 INSTANCE
 output "ACCESS_YOUR_JENKINS_HERE" {
